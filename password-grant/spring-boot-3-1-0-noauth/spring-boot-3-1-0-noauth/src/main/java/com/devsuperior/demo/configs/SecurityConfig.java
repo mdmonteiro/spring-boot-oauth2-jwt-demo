e028c9f35bb110f1d1b828c92d4037a61e25bff1
mmont <mmont@Maycon>
2025-03-07 10:44:46 -0300

    bcrypt implement

--- password-grant/spring-boot-3-1-0-noauth/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/configs/SecurityConfig.java
+++ password-grant/spring-boot-3-1-0-noauth/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/configs/SecurityConfig.java
@@ -3,16 +3,23 @@ package com.devsuperior.demo.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf(csrf -> csrf.disable());
 		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 		return http.build();
+	}
+
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- password-grant/spring-boot-3-1-0-noauth/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$S8D16H/..P3tfXo1puhIMOzSIBE7/XRiEvo5muUB0sXanoDukWr72');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$S8D16H/..P3tfXo1puhIMOzSIBE7/XRiEvo5muUB0sXanoDukWr72');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
